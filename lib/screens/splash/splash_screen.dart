cc19a580288e8f84d9bc8f237fe8e8809bebb899
Samarth Shah <57283918+samarth-shah@users.noreply.github.com>
2022-07-18 01:05:03 +0530

    Whole APP Done 👍

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../size_config.dart';
+import 'components/body.dart';
+
+class SplashScreen extends StatelessWidget {
+  static String routeName = "/splash";
+
+  const SplashScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // You have to call it on your starting screen
+    SizeConfig().init(context);
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
